0dc727d8ed94074bb242931735d754da71293b28
nathanroberts55 <nathan.roberts.15@cnu.edu>
2024-08-17 19:57:04 -0400

    added typescript reference

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,5 +1,6 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
+/// <reference types="svelte-adapter-azure-swa" />
 declare global {
 	namespace App {
 		// interface Error {}
