dd15b050679f5217f6710d01b9f9c01c6220a130
nathanroberts55 <nathan.roberts.15@cnu.edu>
2024-08-17 18:52:51 -0400

    Testing

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,6 @@
 	import 'tailwindcss/tailwind.css';
 	import Navbar from './Navbar.svelte';
 	import Footer from './Footer.svelte';
-	export const prerender = true;
-	export const trailingSlash = 'always';
 </script>
 
 <Navbar />
