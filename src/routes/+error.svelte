946f3e5593c93b3ecdc23874a54be7685c498892
nathanroberts55 <nathan.roberts.15@cnu.edu>
2024-08-17 20:23:32 -0400

    Switching to GH Pages

---
+++ src/routes/+error.svelte
@@ -0,0 +1,13 @@
+<svelte:head>
+	<title>The Basement Yard Bingo | Page Not Found</title>
+</svelte:head>
+
+<div class="mx-auto my-auto flex flex-col">
+	<p class="text-8xl text-center my-6">Page Not Found</p>
+	<p class="text-center">
+		Seems like you're a lil lost there pal. How about you head on back to the Game?
+	</p>
+	<a href="/" class="my-6 mx-auto"
+		><button class="btn btn-primary btn-xs sm:btn-sm md:btn-md lg:btn-lg">Play Bingo</button></a
+	>
+</div>
